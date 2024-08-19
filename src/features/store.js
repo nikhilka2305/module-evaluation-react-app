9fcbe391935261d86470201896e121b8288b637a
nikhilka <nikhilka23@gmail.com>
2024-08-19 10:31:30 +0530

    Working with redux and local storage

--- src/features/store.js
+++ src/features/store.js
@@ -1,8 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactManageReducer from "./contactManagement/contactManageSlice";
+const LOCAL_STORAGE = "contacts";
+const getContacts = () => {
+	if (localStorage.getItem(LOCAL_STORAGE) !== null) {
+		const getItem = JSON.parse(localStorage.getItem(LOCAL_STORAGE));
+		console.log(getItem);
+		return getItem;
+	}
+};
+
+const addContactsMiddleware = ({ getState }) => {
+	return (next) => (action) => {
+		const result = next(action);
+		localStorage.setItem(LOCAL_STORAGE, JSON.stringify(getState()));
+		return result;
+	};
+};
 
 export default configureStore({
 	reducer: {
 		contacts: contactManageReducer,
 	},
+	preloadedState: getContacts(),
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(addContactsMiddleware),
 });
